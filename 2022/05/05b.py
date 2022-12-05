0d108cbe13efa1a8c9b091ff35a0c96f7468465a
Tommy Garvin <tommygarvin@gmail.com>
2022-12-05 14:20:16 -0500

    Day 05

---
+++ 2022/05/05b.py
@@ -0,0 +1,44 @@
+# nested list representing stacks of crates
+stacks = [
+    ["None"],
+    ["W", "M", "L", "F"],
+    ["B", "Z", "V", "M", "F"],
+    ["H", "V", "R", "S", "L", "Q"],
+    ["F", "S", "V", "Q", "P", "M", "T", "J"],
+    ["L", "S", "W"],
+    ["F", "V", "P", "M", "R", "J", "W"],
+    ["J", "Q", "C", "P", "N", "R", "F"],
+    ["V", "H", "P", "S", "Z", "W", "R", "B"],
+    ["B", "M", "J", "C", "G", "H", "Z", "W"]
+]
+
+# test data
+# stacks = [
+#     ["None"],
+#     ["Z", "N"],
+#     ["M", "C", "D"],
+#     ["P"]
+# ]
+
+
+def move_crane(pattern):            # function to move the crane
+    count = pattern[0]              # pull data out of pattern list
+    source = pattern[1]             # pull data out of pattern list
+    destination = pattern[2]        # pull data out of pattern list
+    claw = stacks[source][-count:]  # claw picks up count mount of crates
+    del stacks[source][-count:]     # delete same amount of crats from source
+    stacks[destination] += claw     # add those crates to destination
+
+
+with open("../input/05.txt", "r") as file:  # read from file
+    for line in file:                       # loop through lines in file
+        pattern = []                        # an empty list to hold pattern
+        for s in line.split():              # split up the line into strings
+            if s.isdigit():                 # if the string is a digit
+                pattern.append(int(s))      # then add it to the pattern list
+        move_crane(pattern)                 # send pattern to crane
+
+result = []                   # an empty list to hold results
+for stack in stacks:          # for each nested list
+    result.append(stack[-1])  # add last item to results
+print(result)                 # print results
