f70e4971a59e234797f9727283074aa2c034b487
Tommy Garvin <tommygarvin@gmail.com>
2024-12-09 17:18:49 -0500

    202403b

--- 2024/03/main.go
+++ 2024/03/main.go
@@ -13,7 +13,7 @@ func main() {
 	input := getInput()
 	//log.Printf("Input: %s", input)
 	part1(input)
-	//part2(input)
+	part2(input)
 }
 
 func handleError(err error) {
@@ -63,6 +63,63 @@ func part1(input string) {
 		// multiply the two numbers and store in products slice
 		product := firstNumber * secondNumber
 		products = append(products, product)
+	}
+	log.Printf("Products: %v", products)
+
+	// sum the products
+	sum := 0
+	for _, product := range products {
+		sum += product
+	}
+	log.Printf("Sum: %d", sum)
+}
+
+func part2(input string) {
+	log.Printf("Input: %s", input)
+	// find all instances and store in slice
+	filterAll := regexp.MustCompile(`(mul\(\d{1,3},\d{1,3}\))|(do\(\))|(don't\(\))`)
+	filterMultiply := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`)
+	filterDigits := regexp.MustCompile(`\d+`)
+	filterDo := regexp.MustCompile(`do\(\)`)
+	filterDont := regexp.MustCompile(`don't\(\)`)
+	matches := filterAll.FindAllString(input, -1)
+	log.Printf("Matches: %s", matches)
+
+	// a slice of the products
+	var products []int
+
+	isInstructionOn := true
+	log.Printf("Instruction is on")
+	for _, match := range matches {
+		log.Printf("Processing: %s", match)
+
+		// check if the match is do() or don't()
+		if filterDo.MatchString(match) {
+			isInstructionOn = true
+			log.Printf("Instruction is on")
+		} else if filterDont.MatchString(match) {
+			isInstructionOn = false
+			log.Printf("Instruction is off")
+		}
+
+		if isInstructionOn {
+			if filterMultiply.MatchString(match) {
+				// extract the two numbers
+				numbers := filterDigits.FindAllString(match, -1)
+
+				log.Printf("Numbers: %s", numbers)
+				// convert the numbers to integers
+				firstNumber, err := strconv.Atoi(numbers[0])
+				handleError(err)
+				log.Printf("First Number: %d", firstNumber)
+				secondNumber, err := strconv.Atoi(numbers[1])
+				handleError(err)
+				log.Printf("Second Number: %d", secondNumber)
+				// multiply the two numbers and store in products slice
+				product := firstNumber * secondNumber
+				products = append(products, product)
+			}
+		}
 	}
 	log.Printf("Products: %v", products)
 
